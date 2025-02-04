d50cdfae5748f1d59dd518ee54e0aea8c4a5ffd1
Luce <thisisluce@gmail.com>
2025-02-04 18:10:37 -0500

    create TestimonialCard component

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -5,9 +5,40 @@ import ReactDOM from "react-dom/client";
 import "../css/style.css";
 
 const App = () => {
+  const testimonial = {
+    fullName: "Sarah Dole",
+    handle: "@sarahdole",
+    profilePicSrc: "../../img/profile-thumbnail.png",
+    blurb:
+      "I've been searching for high-quality abstract images for my design projects, and I'm thrilled to have found this platform. The variety and depth of creativity are astounding!",
+  };
+
   return (
-    <div className="flex flex-col min-h-screen w-full m-0 p-0 bg-gradient-to-br from-gray-50 to-[#d2d6db]">
-      <h1>Hello, React!</h1>
+    <div className="flex flex-col min-h-screen w-full p-0 items-center justify-center bg-gradient-to-br from-gray-50 to-[#d2d6db]">
+      <TestimonialCard testimonial={testimonial} />
+    </div>
+  );
+};
+
+const TestimonialCard = ({ testimonial }) => {
+  const { fullName, handle, profilePicSrc, blurb } = testimonial;
+  return (
+    <div className="w-[340px] flex flex-col gap-4 bg-white p-6 rounded-lg">
+      <div className="flex gap-4">
+        <img
+          src={profilePicSrc}
+          alt="profile picture"
+          className="w-12 h-12 object-cover"
+        />
+        <div className="flex flex-col gap-px grow">
+          <span className="font-semibold text-lg text-neutral-900">
+            {fullName}
+          </span>
+          <span className="font-normal text-sm text-neutral-600">{handle}</span>
+        </div>
+      </div>
+
+      <span className="font-normal text-base text-neutral-600">{blurb}</span>
     </div>
   );
 };
