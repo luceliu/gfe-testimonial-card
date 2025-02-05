563dee78bc31438cbf7d6c11ecc63ca83bf361e4
Luce <thisisluce@gmail.com>
2025-02-05 17:21:19 -0500

    set base to be repo name to fix broken img link?

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   loader: { ".js": "jsx" },
-  base: "./",
+  base: "/gfe-testimonial-card/",
   build: {
     outDir: "docs", // Output files to the `docs` directory
   },
