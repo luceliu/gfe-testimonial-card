587a01cdaf85799b3644d2310b98fcc05c84badf
Luce <thisisluce@gmail.com>
2025-02-05 17:05:20 -0500

    put assets in public root dir

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -8,7 +8,7 @@ const App = () => {
   const testimonial = {
     fullName: "Sarah Dole",
     handle: "@sarahdole",
-    profilePicSrc: "../../img/profile-thumbnail.png",
+    profilePicSrc: "/img/profile-thumbnail.png",
     blurb:
       "I've been searching for high-quality abstract images for my design projects, and I'm thrilled to have found this platform. The variety and depth of creativity are astounding!",
   };
