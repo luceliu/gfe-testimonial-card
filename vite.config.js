0081162b869616282ebf8340c2eb691d3a76345b
Luce <thisisluce@gmail.com>
2025-02-05 16:58:16 -0500

    configure vite's build path

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   loader: { ".js": "jsx" },
+  base: "./",
+  build: {
+    outDir: "docs", // Output files to the `docs` directory
+  },
 });
